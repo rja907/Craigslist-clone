6c994365f3b066a95bb1d72a6963542c987968d5
Raj <kmrrajj@gmail.com>
2018-06-11 01:43:27 +0530

    added a terms page

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -5,4 +5,6 @@ class PagesController < ApplicationController
   end
   def safety
   end
+  def terms
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'categories#index'
   match '/help', to:'pages#help', via: :get
   match '/scams', to:'pages#scams', via: :get
-  match '/safety', to:'pages#safety', via: :get 
+  match '/safety', to:'pages#safety', via: :get
+  match '/terms', to:'pages#terms', via: :get
 end
