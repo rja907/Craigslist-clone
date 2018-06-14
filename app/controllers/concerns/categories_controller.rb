293817997f997f560cc5835c67aaa6736886a053
Raj <kmrrajj@gmail.com>
2018-06-14 15:30:45 +0530

    added personals category links

--- app/controllers/concerns/categories_controller.rb
+++ app/controllers/concerns/categories_controller.rb
@@ -4,5 +4,6 @@ class CategoriesController < ApplicationController
     @community = @categories[0]
     @housing = @categories[1]
     @forsale = @categories[3]
+    @personals = @categories[2]
   end
 end
