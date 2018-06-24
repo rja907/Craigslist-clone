f5304edd6e8a61e510482a1ed433dc28781c5364
Raj <kmrrajj@gmail.com>
2018-06-24 23:41:27 +0530

    added code for searching in the listings model

--- app/models/concerns/listing.rb
+++ app/models/concerns/listing.rb
@@ -8,5 +8,11 @@ class Listing < ActiveRecord::Base
   def full_address
     [city, state, zipcode].join(', ')
   end
- 
+
+  def self.search(params)
+    listings = Listing.where(category_id: params[:category].to_i)
+    listings = listings.where("title like ? or description like ?", params[:search], params[:search]) if params[:search].present?
+    listings = listings.near(params[:location], 20) if params[:location].present?
+    listings 
+  end
 end
