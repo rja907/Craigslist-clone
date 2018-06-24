d90059ed5ca6483cd45949a6c95b7417bc5beea7
Raj <kmrrajj@gmail.com>
2018-06-24 23:50:10 +0530

    added a route for listings search

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   resources :categories do
     resources :subcategories
   end
-  resources :listings
+  resources :listings do
+    collection do
+      get 'search'
+    end
+  end
   root 'categories#index'
   match '/help', to:'pages#help', via: :get
   match '/scams', to:'pages#scams', via: :get
