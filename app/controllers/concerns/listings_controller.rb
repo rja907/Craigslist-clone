d02846e264737839924be1537f7928b7b5e06594
Raj <kmrrajj@gmail.com>
2018-06-24 23:42:10 +0530

    added search to Listing model instance with params

--- app/controllers/concerns/listings_controller.rb
+++ app/controllers/concerns/listings_controller.rb
@@ -10,6 +10,9 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+  def search
+    @listing = Listing.search(params)
+  end
   private
   def listing_params
     params.require(:listing).permit(:title, :description, :city, :state, :zipcode, :category_id, :subcategory_id)
