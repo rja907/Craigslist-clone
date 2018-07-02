a87c16952f050736c655701557d2b6e64b63044c
Raj <kmrrajj@gmail.com>
2018-07-01 19:26:50 -0700

    added string interpolation for sqlite

--- app/models/concerns/listing.rb
+++ app/models/concerns/listing.rb
@@ -11,8 +11,8 @@ class Listing < ActiveRecord::Base
 
   def self.search(params)
     listings = Listing.where(category_id: params[:category].to_i)
-    listings = listings.where("title like ? or description like ?", params[:search], params[:search]) if params[:search].present?
+    listings = listings.where("title like ? or description like ?", "#{params[:search]}", "#{params[:search]}") if params[:search].present?
     listings = listings.near(params[:location], 20) if params[:location].present?
-    listings 
+    listings
   end
 end
