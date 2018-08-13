de159743302ce89a47e8f12cfb7dbc06ee74a458
Raj <kmrrajj@gmail.com>
2018-08-12 23:46:56 -0700

    bug fix

--- app/controllers/concerns/subcategories_controller.rb
+++ app/controllers/concerns/subcategories_controller.rb
@@ -1,6 +1,8 @@
 class SubcategoriesController < ApplicationController
   def show
-    @listings = Listing.where(subcategory_id: params[:id])
+    @listings = Listing.where(subcategory_id: params[:id]).order('created_at DESC')
+    @category = Category.find(params[:category_id])
+    @subcategory = Subcategory.find(params[:id])
   end
   def find_by_category
     category = Category.find(params[:category_id])
